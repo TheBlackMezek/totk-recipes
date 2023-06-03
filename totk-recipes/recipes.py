d886477e809782a1cacde1889e58b085a7e32391
TheBlackMezek <31017211+TheBlackMezek@users.noreply.github.com>
2023-06-02 18:55:24 -0700

    Use __file__ instead of sys.argv[0]

--- totk-recipes/recipes.py
+++ totk-recipes/recipes.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
 import json
-import sys
 from os import path
 
 app = Flask(__name__)
 
-script_path = path.dirname(sys.argv[0])
+script_path = path.dirname(__file__)
 with open(path.join(script_path, 'ingredients.json'), 'r') as ingredients_file:
     ingredients = json.load(ingredients_file)
 with open(path.join(script_path, 'recipes.json'), 'r') as recipes_file:
