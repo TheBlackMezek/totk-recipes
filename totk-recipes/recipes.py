40338be8398726492695484c289088bde3fdd074
TheBlackMezek <31017211+TheBlackMezek@users.noreply.github.com>
2023-06-02 18:45:33 -0700

    Use more consistent path to json files

--- totk-recipes/recipes.py
+++ totk-recipes/recipes.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 import json
+import sys
+from os import path
 
 app = Flask(__name__)
 
-with open('ingredients.json', 'r') as ingredients_file:
+script_path = path.dirname(sys.argv[0])
+with open(path.join(script_path, 'ingredients.json'), 'r') as ingredients_file:
     ingredients = json.load(ingredients_file)
-with open('recipes.json', 'r') as recipes_file:
+with open(path.join(script_path, 'recipes.json'), 'r') as recipes_file:
     recipes = json.load(recipes_file)
 
 @app.route('/')
